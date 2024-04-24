434c2bdab02c304ecb915c2cc7f9a0addfd93610
William Eriksson <william99eriksson@outlook.com>
2024-04-24 13:50:03 +0200

    testat med allsvenska spelare

--- testLightGBM.py
+++ testLightGBM.py
@@ -23,10 +23,12 @@ from keras.layers import Dense
 from IPython.display import display
 
 
-data0=pd.read_csv("C:/Skola/Examensarbete/footballData.csv")
+with pd.option_context('mode.use_inf_as_na', True):
+    data0 = pd.read_csv("C:/Skola/Examensarbete/footballData.csv")
 display(data0[0:3].T)
 data0[['value_eur']].info()
 
+data1 = data0.copy()
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -39,8 +41,9 @@ def labelencoder(df):
             df[c] = lbl.transform(df[c].values)
     return df
 
-data1 = labelencoder(data0)
-
+#swedish_allsvenskan_data = data0[data0['league_name'] == 'Swedish Allsvenskan']
+#data1=labelencoder(swedish_allsvenskan_data)
+data1=labelencoder(data0)
 m=len(data1)
 M=list(range(m))
 random.seed(2021)
@@ -50,10 +53,32 @@ train=data1.iloc[M[0:(m//4)*3]]
 test=data1.iloc[M[(m//4)*3:]]
 
 target=['value_eur']
-trainY=train[target]
-trainX=train.drop(target,axis=1)
-testY=train[target]
-testX=train.drop(target,axis=1)
+#testAllsvenskan=['league_name']
+#trainY=train[target]
+#trainX=train.drop(target,axis=1)
+#trainX=train[testAllsvenskan]
+#testY=train[target]
+#testX=train.drop(target,axis=1)
+#testX=train[testAllsvenskan]
+
+print(data0.head())
+
+print('Start')
+print(data0['league_name'].unique())
+print('End')
+
+# Filtrera data för Swedish Allsvenskan
+#swedish_allsvenskan_data = data0[data0['league_name'] == 'Swedish Allsvenskan']
+attributes = ['potential', 'age', 'overall', 'player_positions', 'international_reputation']
+# Definiera attribut och målvariabler för träningsuppsättningen
+trainY = train[target]
+#trainX = train.drop(target, axis=1)
+trainX = train[attributes]
+
+# Definiera attribut och målvariabler för testuppsättningen
+testY = train[target]
+#testX = train.drop(target, axis=1)
+testX = train[attributes]
 
 df_columns = list(trainX.columns)
 print(df_columns)
@@ -133,20 +158,22 @@ def fit_lgbm(X, y, cv, params: dict=None, verbose: int=50):
         pred_i = clf.predict(x_valid)
         oof_pred[idx_valid] = pred_i
         models.append(clf)
-        print(f'Fold {i} RMSLE: {mean_squared_error(y_valid, pred_i) ** .5:.4f}')
-        print()    
+        mse = mean_squared_error(y_valid, pred_i)  # Calculate MSE
+        rmse = np.sqrt(mse)  # Calculate RMSE from MSE
+        print(f'Fold {i} RMSE: {rmse:.4f}')  # Print RMSE
+        print()
         
     score = mean_squared_error(y, oof_pred) ** .5
     print('-' * 50)
-    print('FINISHED | Whole RMSLE: {:.4f}'.format(score))
+    print('FINISHED | Whole RMSE: {:.4f}'.format(score))
     return oof_pred, models
 
 params = {
     'objective': 'rmse', 
-    'learning_rate': .1,
-    'reg_lambda': 1.,
+    'learning_rate': .05,
+    'reg_lambda': .1,
     'reg_alpha': .1,
-    'max_depth': 5, 
+    'max_depth': -1, 
     'n_estimators': 10000, 
     'colsample_bytree': .5, 
     'min_child_samples': 10,
@@ -154,7 +181,8 @@ params = {
     'subsample': .9,
     'importance_type': 'gain', 
     'random_state': 71,
-    'num_leaves': 62
+    'num_leaves': 62,
+    'verbosity': -1
 }
 
 ydf=trainY
@@ -209,6 +237,6 @@ for i, target_col in enumerate(target):
     ydfi=ydf[target_col]
     y=np.array(ydfi)
     cv = list(fold.split(train_feat_df, y))
-    oof, models = fit_lgbm(train_feat_df.values, y, cv, params=params, verbose=500)
+    oof, models = fit_lgbm(train_feat_df.values, y, cv, params=params)
     fig, ax = visualize_importance(models, train_feat_df)
     ax.set_title(target_col+' Imortance',fontsize=20)
